ecfcad3cc97c0ea922f084de6ef73607cfaf87a2
axect <ax2ct@outlook.com>
2023-12-17 07:20:59 +0900

    Update uniform-gravity-1d-bf

--- reinla/src/bin/uniform_gravity_1d_bruteforce.rs
+++ reinla/src/bin/uniform_gravity_1d_bruteforce.rs
@@ -1,5 +1,6 @@
 use reinla::lagrangian::one_dim::UniformGravity;
 use reinla::lattice::one_dim::Lattice1D;
+use std::env::args;
 
 type E = Lattice1D<L>;
 type L = UniformGravity;
@@ -7,8 +8,10 @@ type L = UniformGravity;
 const M: f64 = 1.0;
 const G: f64 = 2.0;
 
+#[allow(non_snake_case)]
 pub fn main() {
-    let env = E::new(21, 0, 20, 4, L::new(M, G));
+    let N: usize = args().nth(1).unwrap().parse().unwrap();
+    let env = E::new(N+1, 0, N as i64, 3, L::new(M, G));
     let result = env.brute_force();
     println!("{:?}", result);
 }
