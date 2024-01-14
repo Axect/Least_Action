bae3f3f92cd6301ee09a8de12de51bddf0ab5bab
axect <ax2ct@outlook.com>
2024-01-14 23:22:37 +0900

    Add three

--- theory/src/main.rs
+++ theory/src/main.rs
@@ -9,8 +9,10 @@ fn main() {
 
     let y_harmonic = x.fmap(harmonic_half);
     let y_pade = x.fmap(pade);
-    let y_taylor = x.fmap(taylor); 
+    let y_taylor = x.fmap(taylor);
     let y_one = x.fmap(one_node);
+    let y_pade2 = x.fmap(pade2);
+    let y_three = x.fmap(three_node);
 
     let mut df = DataFrame::new(vec![]);
     df.push("x", Series::new(x));
@@ -18,9 +20,12 @@ fn main() {
     df.push("pade", Series::new(y_pade));
     df.push("taylor", Series::new(y_taylor));
     df.push("one", Series::new(y_one));
+    df.push("pade2", Series::new(y_pade2));
+    df.push("three", Series::new(y_three));
 
     df.print();
-    df.write_parquet("data.parquet", CompressionOptions::Uncompressed).unwrap();
+    df.write_parquet("data.parquet", CompressionOptions::Uncompressed)
+        .unwrap();
 }
 
 fn harmonic_half(x: f64) -> f64 {
@@ -31,10 +36,20 @@ fn pade(x: f64) -> f64 {
     (1f64 + 1f64 / 48f64 * x.powi(2)) / (1f64 - 5f64 / 48f64 * x.powi(2))
 }
 
+fn pade2(x: f64) -> f64 {
+    (1f64 + 11f64 / 1008f64 * x.powi(2) + 13f64 * x.powi(4) / 241920f64)
+        / (1f64 + 115f64 * x.powi(2) / 1008f64 + 313f64 * x.powi(4) / 241920f64)
+}
+
 fn taylor(x: f64) -> f64 {
     1f64 + x.powi(2) / 8f64
 }
 
 fn one_node(x: f64) -> f64 {
     (1f64 + 1f64 / 16f64 * x.powi(2)) / (1f64 - 1f64 / 16f64 * x.powi(2))
+}
+
+fn three_node(x: f64) -> f64 {
+    (1f64 + x.powi(2) / 16f64 + x.powi(4) / 1024f64)
+        / (1f64 - 3f64 * x.powi(2) / 16f64 + x.powi(4) / 1024f64)
 }
