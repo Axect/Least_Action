84b0b018c6c53cfdb5d1a92e7297cb1b95d3eb95
axect <ax2ct@outlook.com>
2024-01-19 12:03:23 +0900

    Divide, Conquer and Correct

--- algola/pq_plot.py
+++ algola/pq_plot.py
@@ -14,43 +14,58 @@ end = q_true[-1]
 
 dh = pd.read_parquet('data.parquet')
 
-species = 3
+species = 5
 
 t_hats = [np.linspace(t_start, t_end, 2**i + 1) for i in range(1, species+1)]
-bfs = []
+#bfs = []
 dcs = []
-for i in range(1, 4):
-    bf = np.array(dh[f'bf_{i}'][:2**i-1])
+dccs = []
+for i in range(1, species+1):
+    #bf = np.array(dh[f'bf_{i}'][:2**i-1])
     dc = np.array(dh[f'dc_{i}'][:2**i-1])
-    bf = np.insert(bf, 0, start)
-    bf = np.append(bf, end)
+    dcc = np.array(dh[f'dcc_{i}'][:2**i-1])
+    #bf = np.insert(bf, 0, start)
+    #bf = np.append(bf, end)
     dc = np.insert(dc, 0, start)
     dc = np.append(dc, end)
-    bfs.append(bf)
+    dcc = np.insert(dcc, 0, start)
+    dcc = np.append(dcc, end)
+    #bfs.append(bf)
     dcs.append(dc)
-styles = ['-', '--', ':']
+    dccs.append(dcc)
+styles = ['-', '--', '-', '-.', ':']
 
 pparam = dict(
     xlabel = r'$t$',
     ylabel = r'$q$',
 )
 
-with plt.style.context(["science", "nature"]):
-    fig, ax = plt.subplots()
-    ax.autoscale(tight=True)
-    ax.set(**pparam)
-    ax.plot(t_true, q_true, 'k', label=r'$q_\text{true}~(\omega=1, T=\frac{2}{3}\pi)$', alpha=0.3)
-    for i in range(1, 4):
-        ax.plot(t_hats[i-1], bfs[i-1], styles[i-1], label=rf'$\hat{{q}}_\text{{bf}} ~(N=2^{i} - 1)$', alpha=0.7)
-    ax.legend()
-    fig.savefig('harmonic_bf.png', dpi=600, bbox_inches='tight')
+#with plt.style.context(["science", "nature"]):
+#    fig, ax = plt.subplots()
+#    ax.autoscale(tight=True)
+#    ax.set(**pparam)
+#    ax.plot(t_true, q_true, 'k', label=r'$q_\text{true}~(\omega=1, T=\frac{2}{3}\pi)$', alpha=0.3)
+#    for i in range(1, 4):
+#        ax.plot(t_hats[i-1], bfs[i-1], styles[i-1], label=rf'$\hat{{q}}_\text{{bf}} ~(N=2^{i} - 1)$', alpha=0.7)
+#    ax.legend()
+#    fig.savefig('harmonic_bf.png', dpi=600, bbox_inches='tight')
 
 with plt.style.context(["science", "nature"]):
     fig, ax = plt.subplots()
     ax.autoscale(tight=True)
     ax.set(**pparam)
     ax.plot(t_true, q_true, 'k', label=r'$q_\text{true}~(\omega=1, T=\frac{2}{3}\pi)$', alpha=0.3)
-    for i in range(1, 4):
+    for i in range(1, species+1):
         ax.plot(t_hats[i-1], dcs[i-1], styles[i-1], label=rf'$\hat{{q}}_\text{{dc}} ~(N=2^{i} - 1)$', alpha=0.7)
     ax.legend()
     fig.savefig('harmonic_dc.png', dpi=600, bbox_inches='tight')
+
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(t_true, q_true, 'k', label=r'$q_\text{true}~(\omega=1, T=\frac{2}{3}\pi)$', alpha=0.3)
+    for i in range(1, species+1):
+        ax.plot(t_hats[i-1], dccs[i-1], styles[i-1], label=rf'$\hat{{q}}_\text{{dcc}} ~(N=2^{i} - 1)$', alpha=0.7)
+    ax.legend()
+    fig.savefig('harmonic_dcc.png', dpi=600, bbox_inches='tight')
--- algola/src/main.rs
+++ algola/src/main.rs
@@ -1,49 +1,53 @@
-use std::f64::consts::PI;
-use itertools::{Itertools, repeat_n};
-use peroxide::fuga::*;
+use itertools::{repeat_n, Itertools};
+use peroxide::{fuga::*, traits::float::FloatWithPrecision};
 use rayon::prelude::*;
+use std::f64::consts::PI;
 
 #[allow(non_snake_case)]
 fn main() {
     let A = 0.0;
     let B = 20.0;
-    let dq = 0.5;
-    let node_pool = seq(A + dq, B - dq, dq);
+    let dq = 0.1;
+    let node_pool = seq(A + dq, B, dq);
+    let node_pool = node_pool.fmap(|x| x.round_with_precision(1));
 
     let omega = 1f64;
     let T = PI / 2f64;
     let potential = HarmonicOscillator1D::new(omega);
 
     let t_true = linspace(0, T, 1000);
-    let q_true = t_true.fmap(|t| A * (omega * t).cos() + (B - A * (omega * T).cos()) / (omega *
-        T).sin() * (omega * t).sin());
+    let q_true = t_true.fmap(|t| {
+        A * (omega * t).cos() + (B - A * (omega * T).cos()) / (omega * T).sin() * (omega * t).sin()
+    });
 
     let mut df = DataFrame::new(vec![]);
     df.push("t", Series::new(t_true));
     df.push("q", Series::new(q_true));
     df.print();
 
-    df.write_parquet("true.parquet", CompressionOptions::Uncompressed).unwrap();
+    df.write_parquet("true.parquet", CompressionOptions::Uncompressed)
+        .unwrap();
 
     let mut df = DataFrame::new(vec![]);
 
-    let N_pool = 1 .. 4;
+    let N_pool = 1..6;
     for N in N_pool {
-        let bf_nodes = bruteforce_1d_parallel(&node_pool, 2usize.pow(N as u32) - 1, T, A, B, potential);
-        bf_nodes.print();
-        let dc_nodes = divide_and_conquer_1d(&node_pool, N-1, T, A, B, potential);
+        //let bf_nodes = bruteforce_1d_parallel(&node_pool, 2usize.pow(N as u32) - 1, T, A, B, potential);
+        //bf_nodes.print();
+        let dc_nodes = divide_and_conquer_1d(&node_pool, N - 1, T, A, B, potential);
         dc_nodes.print();
-        let dcc_nodes = divide_and_conquer_and_correct_1d(&node_pool, N-1, T, A, B, potential);
+        let dcc_nodes = divide_and_conquer_and_correct_1d(&node_pool, N - 1, T, A, dq, B, potential);
         dcc_nodes.print();
 
-        df.push(&format!("bf_{N}"), Series::new(bf_nodes));
+        //df.push(&format!("bf_{N}"), Series::new(bf_nodes));
         df.push(&format!("dc_{N}"), Series::new(dc_nodes));
         df.push(&format!("dcc_{N}"), Series::new(dcc_nodes));
     }
 
     df.print();
 
-    df.write_parquet("data.parquet", CompressionOptions::Uncompressed).unwrap();
+    df.write_parquet("data.parquet", CompressionOptions::Uncompressed)
+        .unwrap();
 }
 
 pub trait Potential1D: Clone {
@@ -59,7 +63,10 @@ pub struct HarmonicOscillator1D {
 
 impl HarmonicOscillator1D {
     pub fn new(omega: f64) -> HarmonicOscillator1D {
-        HarmonicOscillator1D { omega, omega2: omega.powi(2) }
+        HarmonicOscillator1D {
+            omega,
+            omega2: omega.powi(2),
+        }
     }
 }
 
@@ -72,13 +79,13 @@ impl Potential1D for HarmonicOscillator1D {
 #[allow(non_snake_case)]
 pub fn action_1d<F: Potential1D>(nodes: &[f64], T: f64, start: f64, end: f64, potential: F) -> f64 {
     let N = nodes.len();
-    let dt = T / ((N+1) as f64);
+    let dt = T / ((N + 1) as f64);
 
     let mut nodes = nodes.to_vec();
     nodes.insert(0, start);
     nodes.push(end);
 
-    let nodes_head = &nodes[0..N+1];
+    let nodes_head = &nodes[0..N + 1];
     let nodes_tail = &nodes[1..];
 
     let mut action = 0.0;
@@ -93,7 +100,18 @@ pub fn action_1d<F: Potential1D>(nodes: &[f64], T: f64, start: f64, end: f64, po
 }
 
 #[allow(non_snake_case)]
-pub fn bruteforce_1d<F: Potential1D>(node_pool: &[f64], N: usize, T: f64, start: f64, end: f64, potential: F) -> Vec<f64> {
+pub fn bruteforce_1d<F: Potential1D>(
+    node_pool: &[f64],
+    N: usize,
+    T: f64,
+    start: f64,
+    end: f64,
+    potential: F,
+) -> Vec<f64> {
+    if node_pool.len() == 1 {
+        return vec![node_pool[0]];
+    }
+
     let mut best_action = std::f64::MAX;
     let mut best_nodes = vec![0f64; N];
     for nodes in node_pool.iter().cloned().combinations(N) {
@@ -107,65 +125,144 @@ pub fn bruteforce_1d<F: Potential1D>(node_pool: &[f64], N: usize, T: f64, start:
 }
 
 #[allow(non_snake_case)]
-pub fn bruteforce_1d_parallel<F: Potential1D + Sync>(node_pool: &[f64], N: usize, T: f64, start: f64, end: f64, potential: F) -> Vec<f64> {
+pub fn bruteforce_1d_parallel<F: Potential1D + Sync>(
+    node_pool: &[f64],
+    N: usize,
+    T: f64,
+    start: f64,
+    end: f64,
+    potential: F,
+) -> Vec<f64> {
     let node_combs = node_pool.iter().cloned().combinations(N).collect_vec();
 
-    node_combs.into_par_iter().map(|nodes| {
-        let action = action_1d(&nodes, T, start, end, potential.clone());
-        (action, nodes.clone())
-    }).max_by(|a, b| b.0.partial_cmp(&a.0).unwrap()).unwrap().1
+    node_combs
+        .into_par_iter()
+        .map(|nodes| {
+            let action = action_1d(&nodes, T, start, end, potential.clone());
+            (action, nodes.clone())
+        })
+        .max_by(|a, b| b.0.partial_cmp(&a.0).unwrap())
+        .unwrap()
+        .1
 }
 
 #[allow(non_snake_case)]
-pub fn divide_and_conquer_1d<F: Potential1D>(node_pool: &[f64], D: usize, T: f64, start: f64, end: f64, potential: F) -> Vec<f64> {
+pub fn divide_and_conquer_1d<F: Potential1D>(
+    node_pool: &[f64],
+    D: usize,
+    T: f64,
+    start: f64,
+    end: f64,
+    potential: F,
+) -> Vec<f64> {
     if D == 0 {
         bruteforce_1d(node_pool, 1, T, start, end, potential.clone())
     } else {
         let q = bruteforce_1d(node_pool, 1, T, start, end, potential.clone());
         let index_q = node_pool.iter().position(|&x| x == q[0]).unwrap();
         let node_1 = &node_pool[0..index_q];
-        let node_2 = &node_pool[index_q+1..];
+        let node_2 = &node_pool[index_q + 1..];
 
-        let best_node_1 = divide_and_conquer_1d(node_1, D - 1, T / 2f64, start, q[0], potential.clone());
-        let best_node_2 = divide_and_conquer_1d(node_2, D - 1, T / 2f64, q[0], end, potential.clone());
-        best_node_1.into_iter().chain(q).chain(best_node_2).collect()
+        let best_node_1 =
+            divide_and_conquer_1d(node_1, D - 1, T / 2f64, start, q[0], potential.clone());
+        let best_node_2 =
+            divide_and_conquer_1d(node_2, D - 1, T / 2f64, q[0], end, potential.clone());
+        best_node_1
+            .into_iter()
+            .chain(q)
+            .chain(best_node_2)
+            .collect()
     }
 }
 
 #[allow(non_snake_case)]
-pub fn divide_and_conquer_and_correct_1d<F: Potential1D>(node_pool: &[f64], D: usize, T: f64, start: f64, end: f64, potential: F) -> Vec<f64> {
+pub fn divide_and_conquer_and_correct_1d<F: Potential1D>(
+    node_pool: &[f64],
+    D: usize,
+    T: f64,
+    start: f64,
+    dq: f64,
+    end: f64,
+    potential: F,
+) -> Vec<f64> {
+    if (end - start).abs().round_with_precision(1) <= dq {
+        return vec![(start + end) / 2f64];
+    }
+
     if D == 0 {
         bruteforce_1d(node_pool, 1, T, start, end, potential.clone())
     } else {
         let q = bruteforce_1d(node_pool, 1, T, start, end, potential.clone());
-        let index_q = node_pool.iter().position(|&x| x == q[0]).unwrap();
+        let q_i = q[0].round_with_precision(1);
+        let index_q = node_pool.iter().position(|&x| x == q_i).unwrap();
+
         let node_1 = &node_pool[0..index_q];
-        let node_2 = &node_pool[index_q+1..];
+        let node_2 = &node_pool[index_q + 1..];
+        let best_node_1 = divide_and_conquer_and_correct_1d(
+            node_1,
+            D - 1,
+            T / 2f64,
+            start,
+            dq,
+            q_i,
+            potential.clone(),
+        );
+        let best_node_2 =
+            divide_and_conquer_and_correct_1d(node_2, D - 1, T / 2f64, q_i, dq, end, potential.clone());
+        let mut best_node = best_node_1
+            .clone()
+            .into_iter()
+            .chain(vec![q_i])
+            .chain(best_node_2.clone())
+            .collect_vec();
+        let mut best_action = action_1d(&best_node, T, start, end, potential.clone());
 
-        let best_node_1 = divide_and_conquer_and_correct_1d(node_1, D - 1, T / 2f64, start, q[0], potential.clone());
-        let best_node_2 = divide_and_conquer_and_correct_1d(node_2, D - 1, T / 2f64, q[0], end, potential.clone());
-        let mut best_node = best_node_1.clone().into_iter().chain(q).chain(best_node_2.clone()).collect_vec();
-        let mut action = action_1d(&best_node, T, start, end, potential.clone());
+        let D_isize = D as isize;
+        let d_vec = (-D_isize..0).chain(1..D_isize + 1);
 
-        if index_q > 0 && index_q < node_pool.len() - 1 {
-            let q_in1 = node_pool[index_q - 1];
-            let q_ip1 = node_pool[index_q + 1];
+        for d in d_vec {
+            let index_d = index_q as isize + d;
+            if index_d < 0 || index_d >= node_pool.len() as isize {
+                continue;
+            } else {
+                let index_d = index_d as usize;
+                let q_id = node_pool[index_d];
+                let node_d_1 = &node_pool[0..index_d];
+                let node_d_2 = &node_pool[index_d + 1..];
 
+                let best_node_d_1 = divide_and_conquer_and_correct_1d(
+                    node_d_1,
+                    D - 1,
+                    T / 2f64,
+                    start,
+                    dq,
+                    q_id,
+                    potential.clone(),
+                );
+                let best_node_d_2 = divide_and_conquer_and_correct_1d(
+                    node_d_2,
+                    D - 1,
+                    T / 2f64,
+                    q_id,
+                    dq,
+                    end,
+                    potential.clone(),
+                );
+                let best_node_d = best_node_d_1
+                    .clone()
+                    .into_iter()
+                    .chain(vec![q_id])
+                    .chain(best_node_d_2.clone())
+                    .collect_vec();
+                let action_d = action_1d(&best_node_d, T, start, end, potential.clone());
+
+                if action_d < best_action {
+                    best_action = action_d;
+                    best_node = best_node_d;
+                }
+            }
         }
-        todo!()
 
-        //let indices_1 = best_node_1.iter().map(|x| node_pool.iter().position(|&y| y == *x).unwrap());
-        //let indices_2 = best_node_2.iter().map(|x| node_pool.iter().position(|&y| y == *x).unwrap());
-        //let indices = indices_1.chain(vec![index_q]).chain(indices_2).map(|i| i as i32).collect_vec();
-
-        //let best_nodes: Vec<f64> = best_node_1.into_iter().chain(q).chain(best_node_2).collect();
-
-        //// Correct
-        //let correct_pool = repeat_n(vec![-1, 0, 1], best_nodes.len()).multi_cartesian_product();
-        //correct_pool.map(|cs| {
-        //    let nodes = indices.iter().zip(cs.iter()).map(|(i, c)| *i + c).filter(|&i| i >= 0 && i < best_nodes.len() as i32).map(|i| best_nodes[i as usize]).collect_vec();
-        //    let action = action_1d(&nodes, T, start, end, potential.clone());
-        //    (action, nodes)
-        //}).max_by(|a, b| b.0.partial_cmp(&a.0).unwrap()).unwrap().1
+        best_node
     }
 }
